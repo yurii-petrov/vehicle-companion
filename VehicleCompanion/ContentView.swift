213f12d684e64238f1ed1446d8ad9af827ee47fe
Yurii <pe.ur.ur@gmail.com>
2025-09-17 13:38:50 +0300

    Setup project with SwiftUI and SwiftData
    - Create Xcode project 'VehicleCompanion'
    - Configure iOS 17 deployment target
    - Enable SwiftData for persistence
    - Add .gitignore for Swift/Xcode

---
+++ VehicleCompanion/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  VehicleCompanion
+//
+//  Created by Yurii Petrov on 17.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("VehicleCompanion")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VehicleCompanion/VehicleCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleCompanionApp.swift
+//  VehicleCompanion
+//
+//  Created by Yurii Petrov on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct VehicleCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
