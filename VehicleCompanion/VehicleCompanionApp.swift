56531b3c8509638296a28da317597f54c7460bd6
Yurii <pe.ur.ur@gmail.com>
2025-09-17 15:28:11 +0300

    Setup: configure SwiftData container for Vehicle

--- VehicleCompanion/VehicleCompanionApp.swift
+++ VehicleCompanion/VehicleCompanionApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct VehicleCompanionApp: App {
@@ -13,5 +14,6 @@ struct VehicleCompanionApp: App {
         WindowGroup {
             RootView()
         }
+        .modelContainer(for: [Vehicle.self])
     }
 }
